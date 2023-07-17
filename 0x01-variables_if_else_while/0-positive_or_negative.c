060986c4967d9733d8256aeb9034a3ca48ad8466
ChemTech97 <oyeyemijoshua997@gmail.com>
2023-07-17 10:28:43 -0700

    generating random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,17 @@
  * Description: dtermines n is positive, zero, or negative
  * Return: 0
  */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 		printf("%i is positive\n", n);
-	if else (n == 0)
+	else if (n == 0)
 		printf("%i is zero\n", n);
 	else
-		printf("%i is negative\n", n)
+		printf("%i is negative\n", n);
 	return (0);
 }
